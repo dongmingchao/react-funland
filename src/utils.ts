c4452163476a98696d8ae7412ebc9058c106ec7a
dongmingchao <1657883517@qq.com>
2021-08-05 20:35:40 +0800

    feat: add curry2

---
+++ src/utils.ts
@@ -0,0 +1,19 @@
+export function curry2<A, B, C>(
+  base: (a: A, b: B) => C
+): {
+  (a: A, b: B): C
+  (a: A): (b: B) => C
+} {
+  function wrapSameProps(a: A): (b: B) => C
+  function wrapSameProps(a: A, b: B): C
+  function wrapSameProps(Com: A, Wrapped?: B): ((Wrapped: B) => C) | C {
+    if (Wrapped === undefined) {
+      return function (Wrap: B) {
+        return base(Com, Wrap)
+      }
+    }
+    return base(Com, Wrapped)
+  }
+
+  return wrapSameProps
+}
