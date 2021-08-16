d46a38a7c4497546ab61afca50bb694cc103904c
dongmingchao <1657883517@qq.com>
2021-08-16 18:38:58 +0800

    feat: add curry3

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,43 @@
-export function curry2<A, B, C>(
-  base: (a: A, b: B) => C
-): {
+export type Curry2<A, B, C> = {
   (a: A, b: B): C
   (a: A): (b: B) => C
-} {
-  function wrapSameProps(a: A): (b: B) => C
-  function wrapSameProps(a: A, b: B): C
-  function wrapSameProps(Com: A, Wrapped?: B): ((Wrapped: B) => C) | C {
-    if (Wrapped === undefined) {
-      return function (Wrap: B) {
-        return base(Com, Wrap)
-      }
+}
+
+export function curry2<A, B, C>(base: (a: A, b: B) => C) {
+  function upgrade(a: A): (b: B) => C
+  function upgrade(a: A, b: B): C
+  function upgrade(a: A, b?: B): ((b: B) => C) | C {
+    if (b === undefined) {
+      return (b: B) => base(a, b)
     }
-    return base(Com, Wrapped)
+    return base(a, b)
   }
 
-  return wrapSameProps
+  return upgrade
+}
+
+export type Curry3<A, B, C, D> = {
+  (a: A, b: B, c: C): D
+  (a: A, b: B): (c: C) => D
+  (a: A): Curry2<B, C, D>
+}
+
+export function curry3<A, B, C, D>(
+  base: (a: A, b: B, c: C) => D
+): Curry3<A, B, C, D> {
+  function upgrade(a: A, b: B, c: C): D
+  function upgrade(a: A, b: B): (c: C) => D
+  function upgrade(a: A): Curry2<B, C, D>
+  function upgrade(a: A, b?: B, c?: C) {
+    if (b === undefined) {
+      return curry2<B, C, D>((b, c) => base(a, b, c))
+    }
+    if (c === undefined) {
+      return (c: C) => base(a, b, c)
+    }
+    return base(a, b, c)
+  }
+  return upgrade
 }
 
 export function fold<R>(starter: R, reducers: Array<(f: R) => R>) {
